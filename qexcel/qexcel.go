a616b3f92d92f2ff9f46dad789931bef90c105e0
guyouyin <guyouyin@163.com>
2024-08-06 19:42:56 +0800

    万能写入excel

--- qexcel/qexcel.go
+++ qexcel/qexcel.go
@@ -3,6 +3,7 @@ package qexcel
 import (
 	"errors"
 	"fmt"
+	"github.com/360EntSecGroup-Skylar/excelize"
 	"github.com/tealeg/xlsx"
 	"reflect"
 	"strconv"
@@ -15,7 +16,11 @@ type Tag struct {
 	Column string
 }
 
-func WriteToXlsx(dataList []interface{}, sheetName string, isSaveFile bool) (file *xlsx.File, err error) {
+/*
+写入xlsx
+不支持合并单元格
+*/
+func WriteToXlsxV1(dataList []interface{}, sheetName string, savePath string, isSaveFile bool) (file *xlsx.File, err error) {
 	//1.添加sheet
 	file = xlsx.NewFile()
 	sheet, err := file.AddSheet(sheetName)
@@ -89,12 +94,159 @@ func WriteToXlsx(dataList []interface{}, sheetName string, isSaveFile bool) (fil
 		}
 	}
 	if isSaveFile {
-		file.Save("./test.xlsx")
+		file.Save(savePath)
 	}
 	return file, nil
 }
 
-type User struct {
-	Name string `excel:"title=姓名;width=20;column=A"`
-	Age  int    `excel:"title=年龄;width=50;column=B"`
+/*
+写入xlsx
+支持合并单元格--v2兼容v1
+只支持一层嵌套
+*/
+func WriteToXlsxV2(dataList []interface{}, sheetName string, savePath string, isSaveFile bool) (f *excelize.File, err error) {
+	//1.添加sheet
+	f = excelize.NewFile()
+	f.SetSheetName(sheetName, sheetName)
+	if err != nil {
+		return nil, err
+	}
+
+	//2.处理tag标签
+	vaType := reflect.ValueOf(dataList[0]).Type()
+	titleCount := vaType.NumField()
+	tagMap := map[string]*Tag{}
+	mergeList := make([]string, 0)
+	for i := 0; i < titleCount; i++ {
+		field := vaType.Field(i)
+		if field.Type.Kind() == reflect.Slice {
+			//子结构体
+			elem := field.Type.Elem().Elem()
+			elemCount := elem.NumField()
+			for j := 0; j < elemCount; j++ {
+				s := elem.Field(j).Tag.Get("excel")
+				name := elem.Field(j).Name
+				st := strings.Split(s, ";")
+				if len(st) <= 1 {
+					return nil, errors.New("检查结构体的excel标签")
+				}
+				title := strings.Split(st[0], "=")[1]
+				width, _ := strconv.Atoi(strings.Split(st[1], "=")[1])
+				column := strings.Split(st[2], "=")[1]
+				t := &Tag{
+					Title:  title,
+					Width:  width,
+					Column: column,
+				}
+				tagMap[name] = t
+			}
+		} else {
+			s := field.Tag.Get("excel")
+			st := strings.Split(s, ";")
+			if len(st) <= 1 {
+				return nil, errors.New("检查结构体的excel标签")
+			}
+			title := strings.Split(st[0], "=")[1]
+			width, _ := strconv.Atoi(strings.Split(st[1], "=")[1])
+			column := strings.Split(st[2], "=")[1]
+			fieldName := vaType.Field(i).Name
+			t := &Tag{
+				Title:  title,
+				Width:  width,
+				Column: column,
+			}
+			tagMap[fieldName] = t
+			mergeList = append(mergeList, column)
+		}
+	}
+
+	//3.处理title
+	for _, v := range tagMap {
+		f.SetCellValue(sheetName, fmt.Sprintf("%s%d", v.Column, 1), v.Title)
+		f.SetColWidth(sheetName, v.Column, v.Column, float64(v.Width))
+	}
+
+	//4.处理数据
+	row := 2
+	for i := 0; i < len(dataList); i++ {
+		maxElemSliceCount := 0
+		lastRow := 0
+		data := dataList[i]
+		va := reflect.ValueOf(data)
+		vaTyp := va.Type()
+		titleCount2 := va.NumField()
+		isz := false
+		for j := 0; j < titleCount2; j++ {
+			field := vaTyp.Field(j)
+			if field.Type.Kind() == reflect.Slice {
+				//子结构体数组
+				lastRow = row
+				elemSliceObj := reflect.ValueOf(data).Field(j)
+				elemSliceCount := elemSliceObj.Len()
+				if elemSliceCount > maxElemSliceCount {
+					maxElemSliceCount = elemSliceCount
+					isz = true
+				}
+				for x := 0; x < elemSliceCount; x++ {
+					elemObj := elemSliceObj.Index(x).Elem()
+					elemObjCount := elemObj.NumField()
+					// 遍历切片元素的字段
+					for k := 0; k < elemObjCount; k++ {
+						elemField := elemObj.Field(k)
+						elemFieldType := elemObj.Type().Field(k)
+						tagInfo, ok := tagMap[elemFieldType.Name]
+						if !ok {
+							continue
+						}
+						f.SetCellValue(sheetName, fmt.Sprintf("%s%d", tagInfo.Column, lastRow), elemField.Interface())
+					}
+					lastRow++
+				}
+			} else {
+				//主结构
+				fileName := field.Name
+				vaField := va.Field(j)
+				fileValue := vaField.Interface()
+				tagInfo, ok := tagMap[fileName]
+				if !ok {
+					continue
+				}
+				pos := fmt.Sprintf("%s%d", tagInfo.Column, row)
+				f.SetCellValue(sheetName, pos, fileValue)
+			}
+		}
+		//preRow := row
+		if isz {
+			isz = false
+			row = row + maxElemSliceCount
+			//合并单元格
+			for _, v := range mergeList {
+				merSta := fmt.Sprintf("%s%d", v, row-maxElemSliceCount)
+				merEnd := fmt.Sprintf("%s%d", v, row-1)
+				f.MergeCell(sheetName, merSta, merEnd)
+			}
+		} else {
+			row++
+		}
+	}
+
+	//5.增加样式--左右上下居中
+	style, err := f.NewStyle(`{"alignment":{"horizontal":"center","vertical":"center"}}`)
+	if err != nil {
+		return nil, err
+	}
+	// 设置第一个sheet页的A到Z的单元格的样式
+	for col := 'A'; col <= 'Z'; col++ {
+		for i := 1; i <= row; i++ {
+			cell := string(col) + fmt.Sprintf("%d", i)
+			f.SetCellStyle(sheetName, cell, cell, style)
+		}
+	}
+
+	if isSaveFile {
+		if err = f.SaveAs(savePath); err != nil {
+			return nil, err
+		}
+	}
+	return f, nil
 }
--- qexcel/qexcel_test.go
+++ qexcel/qexcel_test.go
@@ -24,7 +24,88 @@ func TestWriteToXlsx(t *testing.T) {
 	list := []interface{}{}
 	list = append(list, jeff, chary)
 
-	file, err := WriteToXlsx(list, "Sheet1", false)
+	file, err := WriteToXlsxV1(list, "Sheet1", "./test.xlsx", true)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(file)
+}
+
+func TestWriteToXlsxV2(t *testing.T) {
+	type Info struct {
+		Size    int    `excel:"title=面积;width=20;column=D"`
+		Address string `excel:"title=地址;width=20;column=E"`
+	}
+	type Like struct {
+		Desc string `excel:"title=爱好;width=40;column=F"`
+	}
+	type Friend struct {
+		Friend string `excel:"title=朋友;width=40;column=G"`
+	}
+	type User struct {
+		Name   string `excel:"title=姓名;width=20;column=A"`
+		Age    int    `excel:"title=年龄;width=40;column=B"`
+		Email  string `excel:"title=身份证;width=30;column=C"`
+		House  []*Info
+		Like   []*Like
+		Friend []*Friend
+	}
+
+	h1 := &Info{
+		Size:    100,
+		Address: "上海市浦东新区东方明珠",
+	}
+	h2 := &Info{
+		Size:    90,
+		Address: "上海市xxxooo",
+	}
+	h3 := &Info{
+		Size:    80,
+		Address: "北京市xxxooo",
+	}
+	f1 := &Like{
+		Desc: "打球",
+	}
+	f2 := &Like{
+		Desc: "泡澡",
+	}
+	jeffList := make([]*Info, 0)
+	jeffList = append(jeffList, h1)
+	jeffList = append(jeffList, h2)
+	jeffList = append(jeffList, h3)
+
+	frList := make([]*Like, 0)
+	frList = append(frList, f1)
+	frList = append(frList, f2)
+
+	fList := make([]*Friend, 0)
+	for i := 0; i < 10; i++ {
+		p1 := Friend{
+			Friend: fmt.Sprintf("p%d", i),
+		}
+		fList = append(fList, &p1)
+	}
+
+	jeff := User{
+		Name:  "jeff",
+		Age:   18,
+		Email: "12312312",
+		House: jeffList,
+		Like:  frList,
+	}
+	chary := User{
+		Name:   "小明",
+		Age:    20,
+		Email:  "xxxooo",
+		Like:   frList,
+		Friend: fList,
+	}
+	list := []interface{}{}
+	list = append(list, jeff)
+	list = append(list, chary)
+
+	file, err := WriteToXlsxV2(list, "Sheet1", "./test1.xlsx", true)
 	if err != nil {
 		fmt.Println(err)
 		return
