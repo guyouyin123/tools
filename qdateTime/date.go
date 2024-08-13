5e0992e50e2d255a80ed594a6b45eb50c658db02
guyouyin <guyouyin@163.com>
2024-08-13 11:20:41 +0800

    GetRecentWeekDates

--- qdateTime/date.go
+++ qdateTime/date.go
@@ -2,14 +2,14 @@ package qdateTime
 
 import "time"
 
-// 获取时间的零点时间
+// GetZeroTime 获取时间的零点时间
 func GetZeroTime(timestamp int64) time.Time {
 	t := time.Unix(timestamp, 0)
 	zeroTime := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, time.UTC)
 	return zeroTime
 }
 
-// 时间分布--取时间范围的点(以天为单位)，首位一个点，中间点平均分布,如不均匀则更靠近endTime的时间
+// GetDates 时间分布--取时间范围的点(以天为单位)，首位一个点，中间点平均分布,如不均匀则更靠近endTime的时间
 // number 采点的数量
 func GetDates(startTime, endTime int64, number int) []string {
 	// 计算时间差,单位是秒
@@ -38,4 +38,23 @@ func GetDates(startTime, endTime int64, number int) []string {
 		dates[i] = time.Unix(t, 0).Format("2006-01-02")
 	}
 	return dates
+}
+
+// GetRecentWeekDates 获取当前时间前n个礼拜时间段--周天-周六
+func GetRecentWeekDates(n int) [][]string {
+	// 获取当前时间
+	now := time.Now()
+	// 初始化结果切片
+	result := make([][]string, 0, n)
+	// 循环最近n个礼拜
+	for i := 0; i < n; i++ {
+		// 获取当前礼拜的第一天和最后一天
+		firstDay := now.AddDate(0, 0, -(int(now.Weekday())+7)%7-n*7)
+		lastDay := firstDay.AddDate(0, 0, 6)
+		// 将当前礼拜的起止日期添加到结果切片
+		result = append(result, []string{firstDay.Format("2006-01-02"), lastDay.Format("2006-01-02")})
+		// 移到下一个礼拜
+		now = now.AddDate(0, 0, 7)
+	}
+	return result
 }
