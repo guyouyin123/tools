9333793f315c3de47a4e1890c9e6e0126225082a
guyouyin <guyouyin@163.com>
2024-09-13 16:57:37 +0800

    增加zip解压缩

--- qzip/qzip.go
+++ qzip/qzip.go
@@ -6,10 +6,17 @@ import (
 	"github.com/guyouyin123/tools/qstring"
 	"io"
 	"os"
+	"path/filepath"
 )
 
 // AddFilesToZip 批量将多个文件添加到 ZIP 文件
 func AddFilesToZip(filePaths []string, zipFileName, dirName string) error {
+	l := qstring.RsplitN(zipFileName, "/", 1)
+	baseName := l[0]
+	if len(l) == 2 {
+		baseName = l[1]
+	}
+
 	// 创建 ZIP 文件
 	zipFile, err := os.Create(zipFileName)
 	if err != nil {
@@ -27,7 +34,10 @@ func AddFilesToZip(filePaths []string, zipFileName, dirName string) error {
 		if len(l) == 2 {
 			fileName = l[1]
 		}
-		customName := fmt.Sprintf("%s/%s", dirName, fileName)
+		customName := fmt.Sprintf("%s/%s/%s", baseName, dirName, fileName)
+		if dirName == "" {
+			customName = fmt.Sprintf("%s/%s", baseName, fileName)
+		}
 		// 打开要压缩的文件
 		file, err := os.Open(filePath)
 		if err != nil {
@@ -44,6 +54,42 @@ func AddFilesToZip(filePaths []string, zipFileName, dirName string) error {
 		if err != nil {
 			return err
 		}
+	}
+	return nil
+}
+
+func Unzip(src string, dest string) error {
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+	// 创建目标目录
+	os.MkdirAll(dest, 0755)
+	// 遍历 ZIP 文件中的每个文件
+	for _, f := range r.File {
+		fpath := filepath.Join(dest, f.Name)
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(fpath, f.Mode())
+			continue
+		}
+		if err := os.MkdirAll(filepath.Dir(fpath), 0755); err != nil {
+			return err
+		}
+		outFile, err := os.OpenFile(fpath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, f.Mode())
+		if err != nil {
+			return err
+		}
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+		_, err = io.Copy(outFile, rc)
+		if err != nil {
+			return err
+		}
+		outFile.Close()
+		rc.Close()
 	}
 	return nil
 }
--- qzip/qzip_test.go
+++ qzip/qzip_test.go
@@ -7,8 +7,8 @@ import (
 
 func TestAddFileToZip(t *testing.T) {
 	files := []string{"/Users/jeff/myself/tools/qzip/test1你好.txt", "/Users/jeff/myself/tools/qzip/test2.txt"}
-	zipFileName := "/Users/jeff/myself/tools/qzip/output.zip" //压缩包名称
-	dirName := "Jeff"                                         //压缩包内部目录
+	zipFileName := "/Users/jeff/myself/tools/qzip/导出数据.zip" //压缩包名称
+	dirName := "123"                                        //压缩包内部目录，内部无需分目录为空字符
 
 	err := AddFilesToZip(files, zipFileName, dirName)
 	if err != nil {
@@ -16,4 +16,16 @@ func TestAddFileToZip(t *testing.T) {
 	}
 
 	fmt.Println("压缩完成:", zipFileName)
+}
+
+func Test_Unzip(t *testing.T) {
+	zipFile := "/Users/jeff/myself/tools/qzip/导出数据.zip" //压缩包名称
+	destDir := "./output"                               // 解压缩目标目录
+
+	err := Unzip(zipFile, destDir)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("Unzip completed successfully.")
+	}
 }
